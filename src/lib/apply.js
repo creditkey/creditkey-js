7d1c943003a6423e655888a3c2b53e9a7fdd8988
Eben Goodman <eben.goodman@gmail.com>
2018-12-27 09:09:30 -0500

    throw error when loading apply without a public key

--- src/lib/apply.js
+++ src/lib/apply.js
@@ -5,6 +5,10 @@ import { api } from '../utils/platform';
 const width = window.screen.availWidth;
 
 const apply = (key, type = 'modal', platform = 'production') => {
+  if (!key) {
+    throw new Error('API public key required.');
+  }
+
   // always use redirect for small devices
   if (width <= 479) return redirect(source);
 
