3d43cdd9e018087e0c1d933da0165e78d48a46a8
Eben Goodman <eben.goodman@gmail.com>
2019-01-29 16:09:27 -0500

    version bump

--- src/lib/checkout.js
+++ src/lib/checkout.js
@@ -5,9 +5,6 @@ const height = window.screen.availHeight;
 const width = window.screen.availWidth;
 
 const checkout = (source, type = 'modal') => {
-  // always use redirect for small devices
-  //if (width <= 479) return redirect(source);
-
   if (type.toLowerCase() === 'modal') {
     return modal(source);
   } else if (type.toLowerCase() === 'redirect') {
