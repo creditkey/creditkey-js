07b0ad409973aa8fc9909e95a8ba5a7a2a095562
Eben Goodman <eben.goodman@gmail.com>
2019-08-16 13:42:56 -0400

    revert build changes

--- umd/creditkey-js.js
+++ umd/creditkey-js.js
@@ -1,5 +1,5 @@
 /*!
- * creditkey-js v1.0.51 - https://www.creditkey.com
+ * creditkey-js v1.0.52 - https://www.creditkey.com
  * MIT Licensed
  */
 (function webpackUniversalModuleDefinition(root, factory) {
