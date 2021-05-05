9b3fd8970f83e43a301eccac5f80993136774f41
Eben Goodman <eben.goodman@gmail.com>
2021-05-04 21:10:08 -0400

    fix modal pdp post message callback binding

--- src/lib/components/modal-pdp-banner.js
+++ src/lib/components/modal-pdp-banner.js
@@ -7,9 +7,15 @@ const modalPdpBanner = url => {
 }
 
 window.addEventListener('message', function (e) {
-  if (!e || !e.data || !e.data.action) return false;
+  let data;
 
-  const data = JSON.parse(e.data);
+  if (!e || !e.data) return false;
+
+  try {
+    data = JSON.parse(e.data);
+  } catch (e) {
+    return false;
+  }
 
   if (data.action === 'pdp' && data.options.public_key) {
     const charges = new Charges(data.options.charges ? data.options.charges : '0, 0, 0, 0, 0'.split(','));
