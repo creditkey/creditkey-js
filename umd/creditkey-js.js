606b19e83f91fa17c43c2bd7b2ad634f1367ee28
Eben Goodman <eben.goodman@gmail.com>
2021-05-06 14:11:25 -0400

    updated build

--- umd/creditkey-js.js
+++ umd/creditkey-js.js
@@ -1,5 +1,5 @@
 /*!
- * creditkey-js v1.0.83 - https://www.creditkey.com
+ * creditkey-js v1.0.84 - https://www.creditkey.com
  * MIT Licensed
  */
 (function webpackUniversalModuleDefinition(root, factory) {
