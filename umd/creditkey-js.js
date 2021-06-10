18272ff0d24bbcbe4a8ade78c99ad67cea3ee959
cjdeg <cj@benfredaconsulting.com>
2021-06-10 16:31:41 -0400

    added no scroll to another iframe element

--- umd/creditkey-js.js
+++ umd/creditkey-js.js
@@ -18257,7 +18257,7 @@ var Charges = /*#__PURE__*/function () {
 
 
 var modalPdpBanner = function modalPdpBanner(url) {
-  var iframe = "<div className=\"iframe-container\"><iframe id=\"creditkey-pdp-iframe\" src=\"" + url + "\"></iframe></div>";
+  var iframe = "<div className=\"iframe-container\"><iframe scrolling=\"no\" id=\"creditkey-pdp-iframe\" src=\"" + url + "\"></iframe></div>";
   return iframe;
 };
 
